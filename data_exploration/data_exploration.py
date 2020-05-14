f4ca4ff34916626a23815b916924e883692c1832
David Liedtka <dliedtka@gmail.com>
2020-05-14 16:15:05 -0700

    renamed

--- data_exploration/data_exploration.py
+++
@@ -1,8 +0,0 @@
-import json
-
-# 10,435,705 unique keys
-key_list = json.load(open("../../data/key_list.json", "r"))
-
-# see how long it takes to access each key
-
-# run experiments over subset of key list?
